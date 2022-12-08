cdb96901e33d90430a5f71cfef884b6a810a8bc2
dpdahal <dp48dahal@gmail.com>
2022-12-08 13:59:19 +0545

    update

---
+++ Introduction/introduction.py
@@ -0,0 +1,56 @@
+# print("hello")
+
+# print("hello", 20, True)
+
+# print(123)
+
+# print("hello")
+
+# name = 'sophia'
+# print(name)
+
+# what is a variable?
+# a variable is a container for a value
+# rules for naming variables
+# 1. can't start with a number
+# 2. can't contain spaces
+# 3. can't contain special characters
+# 4. can't be a python keyword
+# h1= 'hello'
+
+# _
+
+# username='ram'
+# print(username)
+
+# userName: camel case
+# user_name: snake case
+# UserName: Pascal case
+
+# name = 'sophia'
+# age = 20
+# print("My user name {} age {}".format(name, age))
+# print(f"My user name {name} age {age}")
+# print("my name is "+name + str(age))
+# print(name,age)
+# print(age)
+
+# mother's
+
+# print("mother's")
+# print('mother\'s')
+
+# "mother's"
+
+# print("hello python",end=" ?")
+
+# name = input("enter your name: ")
+# age = input("enter your age: ")
+#
+# print(f"my name is {name} and age {age}")
+
+# x = int(input("Enter x: "))
+# print(type(x))
+# y = int(input("Enter y: "))
+# x =int(x)
+# print(x+y)
